d9d186e3b7551eb4912e4279148994eb8a342d1d
rbettN <bettoni.rafa@gmail.com>
2021-02-26 21:08:53 -0500

    lesson165: Made a post clickable for selection

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ import post from '../../components/Post/Post';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
@@ -26,10 +27,18 @@ class Blog extends Component {
                 this.setState({posts: updatedPosts});
             });
     }
+
+    postSelectedHandler = (postId) => {
+        this.setState({selectedPostId: postId});
+    }
     
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>;
         });
         return (
             <div>
@@ -37,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />

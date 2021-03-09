c270ded5a324a349ff043d0d37a967bac4f700d2
rbettN <bettoni.rafa@gmail.com>
2021-03-08 23:59:06 -0500

    lesson204-205: Using Switch component to load a single route at a time in Bloj.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 //import axios from '../../axios'
 /*The Route component needs to be imported for routing*/
 /*The Link component allows the creation of Links for switching pages (routing).
-The NavLink is similar to Link but it has extra props that allows styling for the active route*/
-import { Route, NavLink } from 'react-router-dom';
+The NavLink is similar to Link but it has extra props that allows styling for the active route.
+The Switch component tells react router which Route to load (the first one that matches), i.e.,
+only the first match is loaded.*/
+
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost';
-import Post from './FullPost/FullPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -39,8 +42,10 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/> 
                 <Route path="/" render={() => <h1>Home 2</h1>}/>*/}
                 <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/:id" exact component={Post}/>
+                <Switch>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
 
             </div>
         );
